7bc3364a3d01de591749c3502dce6ac57adde5ae
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-19 20:38:16 -0300

    Fixed paths

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,32 @@
-import { Container } from "../components/container/styles";
-import { Header}  from "../components/header";
-import UserContainer from "../components/userContainer";
-import { ProfilePicture } from "../components/userPicture/styles";
+import React, { useContext } from 'react';
+import { useQuery } from '../hooks/useQuery';
 
-function home() {
-    return(
+import Container from '../components/container';
+import Header from '../components/header';
+import UserContainer from '../components/userContainer';
+import UserPicture from '../components/userPicture';
+import UserDetails from '../components/userDetails';
+import UserNumbers from '../components/userNumbers';
+
+import { context } from '../context';
+
+export default function Home() {
+    const ctx = useContext(context);
+    const query = useQuery();
+
+    return (
         <Container>
-            <Header/>
+            <Header username={query.get('username')} />
+
             <UserContainer>
-                <ProfilePicture src="https://avatars.githubusercontent.com/u/109322475?v=4" alt="image-test" />
+                {ctx.userData?.name ?
+                    <React.Fragment>
+                        <UserPicture url={ctx.userData?.avatar_url} />
+                        <UserDetails name={ctx.userData?.name} username={ctx.userData?.login} bio={ctx.userData?.bio} />
+                        <UserNumbers repos={ctx.userData?.public_repos} followers={ctx.userData?.followers} following={ctx.userData?.following} />
+                    </React.Fragment>
+                : undefined }
             </UserContainer>
         </Container>
-    )
-} 
-
-export default home;
+    );
+}
